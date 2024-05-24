c094beec4239df4505ff2cda1f12b8d267432693
hiepnvph28430 <mrhiep165@gmail.com>
2024-05-24 19:16:15 +0700

    update: update giao dien trang chu

--- src/layouts/base/Header.tsx
+++ src/layouts/base/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
             <div className="block md:hidden">
                 <Sheet >
                     <SheetTrigger>
-                        <MenuIcon size={26} className="cursor-pointer" onClick={()=>setOpen(prev=>!prev)}/>
+                        <MenuIcon size={26} className="cursor-pointer" onClick={() => setOpen(prev => !prev)} />
                     </SheetTrigger>
                     <SheetContent side={"left"}>
                         <h2 className="text-left text-xl font-semibold border-b border-neutral-300 pb-3">Danh mục</h2>
@@ -40,26 +40,28 @@ export default function Header() {
                             <li><Link to="/" className="text-sm text-neutral-900">Mua 1 tặng 1</Link></li>
                             <li><Link to="/" className="text-sm text-neutral-900">Hệ thống cửa hàng</Link></li>
                             <li>
-                            <Accordion type="single" collapsible>
-                            <AccordionItem value="item-1" className="bg-transparent border-none text-sm text-neutral-900 p-0 m-0">
-                                <AccordionTrigger className="text-sm text-neutral-900 font-normal no-underline  p-0 m-0">Chính sách</AccordionTrigger>
-                                <AccordionContent className="">
-                                <Link to="/" className="text-sm text-neutral-900 block my-2">Quy Chế</Link>
-                                <Link to="/" className="text-sm text-neutral-900 block my-2">Quy Chế</Link>
-                                </AccordionContent>
-                            </AccordionItem>
-                        </Accordion>
+                                <Accordion type="single" collapsible>
+                                    <AccordionItem value="item-1" className="bg-transparent border-none text-sm text-neutral-900 p-0 m-0">
+                                        <AccordionTrigger className="text-sm text-neutral-900 font-normal no-underline  p-0 m-0">Chính sách</AccordionTrigger>
+                                        <AccordionContent className="">
+                                            <Link to="/" className="text-sm text-neutral-900 block my-2">Quy Chế</Link>
+                                            <Link to="/" className="text-sm text-neutral-900 block my-2">Quy Chế</Link>
+                                        </AccordionContent>
+                                    </AccordionItem>
+                                </Accordion>
                             </li>
                         </ul>
                     </SheetContent>
                 </Sheet>
             </div>
-            
+
             <div className="md:w-[200px] flex-1 md:flex-none flex justify-center">
-                <img src="https://theme.hstatic.net/200000690551/1001033677/14/logo.png?v=292" alt="" className="w-[100px] md:w-auto object-contain"/>
+                <Link to="/">
+                    <img src="https://theme.hstatic.net/200000690551/1001033677/14/logo.png?v=292" alt="" className="w-[100px] md:w-auto object-contain" />
+                </Link>
             </div>
             <div className="hidden md:block flex-1">
-                <form>   
+                <form>
                     <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                     <div className="relative">
                         <input type="search" id="search" className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-neutral-800 focus:border-neuring-neutral-800 " placeholder="Tìm kiếm sản phẩm" required />
@@ -101,7 +103,7 @@ export default function Header() {
                     <NavigationMenuItem>
                         <Link to="/">
                             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            Hàng mới về
+                                Hàng mới về
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
@@ -112,7 +114,9 @@ export default function Header() {
                     <NavigationMenuItem>
                         <NavigationMenuTrigger className="hover:bg-transparent">Sản phẩm</NavigationMenuTrigger>
                         <NavigationMenuContent className="py-3">
-                            <NavigationMenuLink className="text-sm block p-2 px-5 hover:bg-neutral-100 transition-all cursor-pointer">Nike</NavigationMenuLink>
+                            <Link to="collections/nike">
+                                <NavigationMenuLink className="text-sm block p-2 px-5 hover:bg-neutral-100 transition-all cursor-pointer" >Nike</NavigationMenuLink>
+                            </Link>
                             <NavigationMenuLink className="text-sm block p-2 px-5 hover:bg-neutral-100 transition-all cursor-pointer">Adidas</NavigationMenuLink>
                         </NavigationMenuContent>
                     </NavigationMenuItem>
@@ -123,7 +127,7 @@ export default function Header() {
                     <NavigationMenuItem>
                         <Link to="/">
                             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            Mua 1 tặng 1
+                                Mua 1 tặng 1
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
@@ -134,7 +138,7 @@ export default function Header() {
                     <NavigationMenuItem>
                         <Link to="/">
                             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            Hệ thống cửa hàng
+                                Hệ thống cửa hàng
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
@@ -143,7 +147,7 @@ export default function Header() {
             <NavigationMenu>
                 <NavigationMenuList>
                     <NavigationMenuItem>
-                    <NavigationMenuTrigger className="hover:bg-transparent">Chính sách</NavigationMenuTrigger>
+                        <NavigationMenuTrigger className="hover:bg-transparent">Chính sách</NavigationMenuTrigger>
                         <NavigationMenuContent className="py-3 w-[300px]">
                             <NavigationMenuLink className="whitespace-nowrap text-sm block p-2 px-5 hover:bg-neutral-100 transition-all cursor-pointer">Quy chế hoạt động</NavigationMenuLink>
                             <NavigationMenuLink className="whitespace-nowrap text-sm block p-2 px-5 hover:bg-neutral-100 transition-all cursor-pointer">Chính sách đặt hàng</NavigationMenuLink>
