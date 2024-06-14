286544c37e3ff1ace392a16bf3b8a2a8793ce841
Trinh Vinh Tam <tamtv28092003@gmail.com>
2024-06-14 23:06:23 +0700

    payment

--- src/features/size/_components/ListSize.tsx
+++ src/features/size/_components/ListSize.tsx
@@ -1,38 +1,35 @@
-import { useEffect } from 'react';
-import { useToast } from '../../../components/ui/use-toast';
-import { useSizeMutation } from '@/hooks/useSizeMutation';
-import { getColumns } from './ColumnSize';
-import { DataTable } from './DataTableSize';
-import { useSizeQuery } from '@/hooks/useSizeQuery';
-import { toast } from 'react-toastify';
+import { useSizeMutation } from '@/hooks/useSizeMutation'
+import { useSizeQuery } from '@/hooks/useSizeQuery'
+import { useEffect } from 'react'
+import { getColumns } from './ColumnSize'
+import { DataTable } from './DataTableSize'
 
 const ListSize = () => {
-    const { data, isLoading, isError, error } = useSizeQuery();
+    const { data, isLoading, isError, error } = useSizeQuery()
 
     useEffect(() => {
         if (data) {
-            console.log('API data:', data);
+            console.log('API data:', data)
         }
         if (error) {
-            console.error('Error:', error);
+            console.error('Error:', error)
         }
-    }, [data, error]);
+    }, [data, error])
 
     const { onRemove } = useSizeMutation({
-        action: 'DELETE',
+        action: 'DELETE'
         // onSuccess: () => {
         //     toast.success('Xóa Size Thành Công')
         // }
-    });
+    })
 
-    if (isLoading) return <div>Loading ...</div>;
-    if (isError) return <div>Error ...</div>;
+    if (isLoading) return <div>Loading ...</div>
+    if (isError) return <div>Error ...</div>
 
-    const columns = getColumns(onRemove);
-    const tableData = data?.data || []; // Adjusted based on your response structure
-    console.log("table", tableData);
+    const columns = getColumns(onRemove)
+    const tableData = data?.data || [] // Adjusted based on your response structure
 
-    return <DataTable columns={columns} data={tableData} />;
-};
+    return <DataTable columns={columns} data={tableData} />
+}
 
-export default ListSize;
+export default ListSize
