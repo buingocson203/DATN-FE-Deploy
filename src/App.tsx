e64ec21383d10a34231a3c78310f5bad6a3fc269
hiepnvph28430 <mrhiep165@gmail.com>
2024-07-20 20:42:43 +0700

    feat: create lien he

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './App.css'
 import Routers from './routes'
 import { ToastContainer } from 'react-toastify'
+import ContactButton from './layouts/base/ContactButton'
 
 function App() {
     return (
         <>
-        <Routers />
-        <ToastContainer />
+            <Routers />
+            <ToastContainer />
+            <ContactButton />
         </>
     )
 }
